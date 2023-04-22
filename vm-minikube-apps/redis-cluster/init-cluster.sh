dc6f88a2a64bef2b5fc9d2a2f066861efcf53138
ShaysRebellion <amadeus@MendicantBias.local>
2023-04-22 10:51:20 +0900

    see previous commit message

--- vm-minikube-apps/redis-cluster/init-cluster.sh
+++
@@ -1,7 +0,0 @@
-redis-cli --cluster create --cluster-replicas 1 \
-  $(getent hosts redis-cluster-0.redis-cluster.redis-cluster.svc.cluster.local | awk '{ print $1 }'):6379 \
-  $(getent hosts redis-cluster-1.redis-cluster.redis-cluster.svc.cluster.local | awk '{ print $1 }'):6379 \
-  $(getent hosts redis-cluster-2.redis-cluster.redis-cluster.svc.cluster.local | awk '{ print $1 }'):6379 \
-  $(getent hosts redis-cluster-3.redis-cluster.redis-cluster.svc.cluster.local | awk '{ print $1 }'):6379 \
-  $(getent hosts redis-cluster-4.redis-cluster.redis-cluster.svc.cluster.local | awk '{ print $1 }'):6379 \
-  $(getent hosts redis-cluster-5.redis-cluster.redis-cluster.svc.cluster.local | awk '{ print $1 }'):6379
